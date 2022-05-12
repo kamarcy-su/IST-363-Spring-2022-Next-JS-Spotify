5476b194ed2684bd759bb818553beb9fb7fca105
Katie Marcy <katiemarcy@Katies-MBP.fios-router.home>
2022-05-12 11:42:37 -0400

    added about page

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+import Heading from '../components/Heading';
+
+const AboutPage = () => {
+    return <Layout >
+        <Heading level="1">
+            About
+        </Heading>
+    </Layout>
+}
+export default AboutPage;
